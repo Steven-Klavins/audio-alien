5d73591a708dfb4b46822a2acf47adcc7ea61c76
Steven <stevenklavins94@gmail.com>
2020-06-11 12:14:06 +0100

    Component: added player to new component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import AudioAlien from './Components/audio-alien'
 
 function App() {
   return (
     <div>
-   <h1>Audio Alien</h1>
+      <h1>Audio Alien</h1>
+      <AudioAlien />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/audio-alien.js
@@ -0,0 +1,14 @@
+import React from 'react'
+const jsmediatags = require('jsmediatags')
+
+export class AudioAlien extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2> Player </h2>
+      </div>
+    )
+  }
+}
+
+export default AudioAlien
